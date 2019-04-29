18025f83c47713bf9804a8244cf6dd7019948a7b
liujian8485@outlook.com <liujian68754807>
2019-04-29 14:28:55 +0800

    Calendar.getInstance();

--- java_demo_02/src/main/java/com/alice/utils/DateUtils.java
+++ java_demo_02/src/main/java/com/alice/utils/DateUtils.java
@@ -13,5 +13,16 @@ public class DateUtils {
         canlandar.add(canlandar.YEAR,50 );
         String syncTime=df.format(canlandar.getTime()).toString();
         System.out.println(syncTime);
+
+
+        Calendar cal = Calendar.getInstance();
+        System.out.println(cal);
+        System.out.println("年:" + cal.get(Calendar.YEAR));
+        System.out.println("月:" + (cal.get(Calendar.MONTH) + 1));
+        System.out.println("日:" + cal.get(Calendar.DAY_OF_MONTH));
+        System.out.println("时:" + cal.get(Calendar.HOUR_OF_DAY));
+        System.out.println("分:" + cal.get(Calendar.MINUTE));
+        System.out.println("秒:" + cal.get(Calendar.SECOND));
+
     }
 }
