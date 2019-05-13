32e27770b11ccebdb6a166dff026445bc47e03e6
ShuaichenLau <68754807@qq.com>
2019-05-13 19:40:36 +0800

    test类

---
+++ java_demo_01/src/main/java/com/alice/Hello001.java
@@ -0,0 +1,18 @@
+package com.alice;
+
+public class Hello001 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i++);
+        System.out.println(i++);
+        System.out.println(i++);
+
+        System.out.println(System.currentTimeMillis());
+
+
+        System.out.println(true == false);
+
+
+    }
+}
