1ba23d974f0f35ffdacfb29960488de9fa30713f
ShuaichenLau <68754807@qq.com>
2019-05-24 23:53:12 +0800

    dom4j 获取bean的方式

--- java_demo_01/src/main/java/com/alice/Hello001.java
+++ java_demo_01/src/main/java/com/alice/Hello001.java
@@ -2,14 +2,24 @@ package com.alice;
 
 import com.alice.entity.User;
 import com.alice.service.Uservice;
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.StringUtils;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
 
 public class Hello001 {
 
     public static void main(String[] args) {
-        int i = 0;
-        System.out.println(++i);
-        System.out.println(++i);
+//        int i = 0;
+//        System.out.println(++i);
+//        System.out.println(++i);
 
 //        System.out.println(System.currentTimeMillis());
 //        System.out.println(true == false);
@@ -22,6 +32,53 @@ public class Hello001 {
         user.setAddress("beijing");
         user.setAge(20);
         userService.insertUser(user);
+
+        Hello001 hello001 = new Hello001();
+        hello001.test001();
+
+
+    }
+
+
+    public void test001(){
+        SAXReader saxReader = new SAXReader();
+        try {
+            //读取xml文件
+            Document document = saxReader.read(getResourceAsStream("spring.xml"));
+            // 读取根节点
+            Element rootElement = document.getRootElement();
+            getNodes(rootElement);
+
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getNodes(Element element){
+        System.out.print("获取节点名称: " + element.getName());
+        //获取属性方法
+        List<Attribute> attributes = element.attributes();
+        for (Attribute attribute : attributes) {
+            System.out.println(attribute.getName() + "==" + attribute.getText());
+        }
+        //获取属性value
+        String textTrim = element.getTextTrim();
+        if (StringUtils.isEmpty(textTrim)){
+            System.out.print("textTrim==>" + textTrim);
+        }
+
+        //使用迭代器 子节点信息
+        Iterator<Element> iterator = element.elementIterator();
+        while (iterator.hasNext()){
+            Element next = iterator.next();
+            getNodes(next);
+        }
+    }
+
+
+    public InputStream getResourceAsStream(String xml){
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(xml);
+        return resourceAsStream;
 
     }
 }
