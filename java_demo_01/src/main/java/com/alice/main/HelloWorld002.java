467a5e8abdae74ec670d829dc372d3f03d5092b5
ShuaichenLau <liujian8485@outlook.com>
2019-08-04 17:44:01 +0800

    多线程 atomicInteget

--- java_demo_01/src/main/java/com/alice/main/HelloWorld002.java
+++ java_demo_01/src/main/java/com/alice/main/HelloWorld002.java
@@ -6,10 +6,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class HelloWorld002 {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring2.xml");
-        System.out.println("==============================================================");
-        Uservice userService = (Uservice) applicationContext.getBean("userService");
-        System.out.println(userService);
+
+        new Thread(() -> {
+            ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring2.xml");
+            System.out.println("==============================================================");
+            Uservice userService = (Uservice) applicationContext.getBean("userService");
+            System.out.println(userService);
+        }).start();
 
     }
 }
---
+++ java_demo_01/src/main/java/com/alice/main/Test002.java
@@ -0,0 +1,23 @@
+package com.alice.main;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+class ThreadDemo extends Thread {
+
+    private static final AtomicInteger ATOMIC_INTEGER = new AtomicInteger(10);
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("ThreadName: " + Thread.currentThread().getName() + " || ATOMIC_INTEGER==> " + ATOMIC_INTEGER.getAndIncrement());
+        }
+    }
+}
+
+public class Test002 {
+    public static void main(String[] args) throws InterruptedException {
+        new ThreadDemo().start();
+        new ThreadDemo().start();
+
+    }
+}
