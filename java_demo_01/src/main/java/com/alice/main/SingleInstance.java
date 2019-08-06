52118bc1bd307d2c691ee0ea360777a3f28cde0b
ShuaichenLau <liujian8485@outlook.com>
2019-08-06 13:57:38 +0800

    update lock

--- java_demo_01/src/main/java/com/alice/main/SingleInstance.java
+++ java_demo_01/src/main/java/com/alice/main/SingleInstance.java
@@ -1,12 +1,20 @@
 package com.alice.main;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 双重校验锁
+ *
  * @author liusc
  */
 public class SingleInstance {
 
     public static volatile SingleInstance singleInstance = null;
+
+    public static ResLock resLock = new ResLock();
 
     private SingleInstance() {
     }
@@ -14,13 +22,28 @@ public class SingleInstance {
     public static SingleInstance getInstance() {
 
         if (singleInstance == null) {
-            synchronized (SingleInstance.class) {
-                if (singleInstance == null) {
-                    singleInstance = new SingleInstance();
-                }
+            resLock.lock.lock();
+//        synchronized (SingleInstance.class) {
+            if (singleInstance == null) {
+                singleInstance = new SingleInstance();
             }
+//        }
+            resLock.lock.unlock();
         }
 
         return singleInstance;
     }
+}
+
+
+class ResLock {
+
+    public Lock lock = new ReentrantLock();
+
+    public String userName;
+    public String userSex;
+
+    public boolean flag = false;
+
+
 }
