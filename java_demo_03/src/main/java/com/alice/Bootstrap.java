4b0826230c1257dd0b3573c463454c50a8c47ff2
ShuaichenLau <68754807@qq.com>
2019-08-24 17:48:43 +0800

    add notes

--- java_demo_03/src/main/java/com/alice/Bootstrap.java
+++ java_demo_03/src/main/java/com/alice/Bootstrap.java
@@ -1,6 +1,7 @@
 package com.alice;
 
 import com.alice.datasources.DynamicDataSourceConfig;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -11,6 +12,6 @@ import org.springframework.context.annotation.Import;
 public class Bootstrap {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Bootstrap.class,args);
     }
 }
