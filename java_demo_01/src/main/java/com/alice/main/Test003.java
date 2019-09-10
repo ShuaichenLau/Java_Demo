808cf6e03e973ab58b8d1627b81bfba13702057e
ShuaichenLau <68754807@qq.com>
2019-09-10 21:17:26 +0800

    add notes

--- java_demo_01/src/main/java/com/alice/main/Test003.java
+++ java_demo_01/src/main/java/com/alice/main/Test003.java
@@ -1,11 +1,17 @@
 package com.alice.main;
 
+import java.util.Date;
+
 public class Test003 {
 
     public static void main(String[] args) {
         String str = "test";
         String test = new String("test");
         System.out.println(test == str);
+
+
+        Date date = new Date();
+        System.out.println(date.getTime());
 
 
     }
