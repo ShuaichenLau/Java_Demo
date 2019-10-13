39e9b83bae5e167db7649029cb0bbe3e52432c54
ShuaichenLau <68754807@qq.com>
2019-10-13 23:13:45 +0800

    server socket 通信简单实现

--- java_demo_04/src/main/java/com/alice/utils/SocketServerDemo.java
+++ java_demo_04/src/main/java/com/alice/utils/SocketServerDemo.java
@@ -3,6 +3,7 @@ package com.alice.utils;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * SocketServerDemo
@@ -15,16 +16,20 @@ public class SocketServerDemo {
         try {
 
             ServerSocket serverSocket = new ServerSocket(9098);
-            System.out.println("开始监听");
+            System.out.println("开始监听......");
             Socket socket = serverSocket.accept();  //阻塞线程
-            System.out.println("监听成功");
+            System.out.println("监听成功......");
+
             InputStream inputStream = socket.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            System.out.println(bufferedReader.readLine());
-
             OutputStream outputStream = socket.getOutputStream();
             PrintWriter printWriter = new PrintWriter(outputStream, true);
-            printWriter.println("hello iYuan");
+
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.println(bufferedReader.readLine());
+                printWriter.println(scanner.nextLine());
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
