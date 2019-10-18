c1c98666db1749e1913f7bd3de2dc1adff8a6ab5
ShuaichenLau <68754807@qq.com>
2019-10-18 17:29:03 +0800

    import static 和 Import的区别

--- JavaEEDemo/springboot_hikaricp/src/main/java/com/alice/hikaricp/controller/HelloWorldController.java
+++ JavaEEDemo/springboot_hikaricp/src/main/java/com/alice/hikaricp/controller/HelloWorldController.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.alice.hikaricp.utils.SystemUtil.println;
+import static java.lang.System.out;
 
 /**
  * HelloWorldDemo
@@ -43,28 +44,28 @@ public class HelloWorldController {
         logger.info("userService ==>  " + userService.getClass());
 
         println("import static ");
+        out.println("import static ");
         return "Greetings from Spring Boot! ";
 
 
     }
 
     @RequestMapping("/index")
-    public String indexHtml(Model model){
+    public String indexHtml(Model model) {
 
-        Person single = new Person("hyj",21);
+        Person single = new Person("hyj", 21);
         List<Person> people = new ArrayList<Person>();
-        Person p1 = new Person("dlp",21);
-        Person p2 = new Person("tq",21);
-        Person p3 = new Person("mk",21);
+        Person p1 = new Person("dlp", 21);
+        Person p2 = new Person("tq", 21);
+        Person p3 = new Person("mk", 21);
         people.add(p1);
         people.add(p2);
         people.add(p3);
-        model.addAttribute("singlePerson",single);
-        model.addAttribute("people",people);
+        model.addAttribute("singlePerson", single);
+        model.addAttribute("people", people);
         return "index";
 
     }
-
 
 
 }
