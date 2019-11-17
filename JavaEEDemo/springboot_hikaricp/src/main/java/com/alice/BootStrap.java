c2c4bdb576807afcbc913cb70a09d0a4fb0bfb5f
ShuaichenLau <68754807@qq.com>
2019-11-17 23:28:48 +0800

    SpringBoot 内部扫包优化 减少扫描无用包

--- JavaEEDemo/springboot_hikaricp/src/main/java/com/alice/BootStrap.java
+++ JavaEEDemo/springboot_hikaricp/src/main/java/com/alice/BootStrap.java
@@ -1,14 +1,19 @@
 package com.alice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 /**
  * @author scliuk
  * @date 2019-9-12 10:10:56
  */
-@SpringBootApplication
+//@SpringBootApplication
+@ComponentScan(basePackages = {"com.alice"})
+@EnableAutoConfiguration
+@EnableAsync    //开启异步调用
 public class BootStrap {
 
     public static void main(String[] args) {
