d46583d178e0bbe626ee911bab3345c53a63554a
ShuaichenLau <68754807@qq.com>
2019-11-20 07:21:12 +0800

    SpringBoot AdminUiClient 添加默认访问路径

--- adminuiclient/src/main/java/com/alice/controller/IndexController.java
+++ adminuiclient/src/main/java/com/alice/controller/IndexController.java
@@ -3,12 +3,19 @@ package com.alice.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class IndexController {
 
     @RequestMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/")
+    public String welcome(){
+        return new Date().toString();
     }
 
 }
