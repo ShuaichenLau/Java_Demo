b8760b9e7c25e438a1c571c3720bdbfaccd1d7c8
ShuaichenLau <68754807@qq.com>
2020-01-01 22:38:34 +0800

    filter过滤器拦截xss 更改注解WebFilter

---
+++ JavaDemo/java_demo_01/src/main/java/com/alice/main/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package com.alice.main;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TestHelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(1 << 4);
+        System.out.println(1 << 30);
+
+        new ConcurrentHashMap<String,String>();
+    }
+}
