eabf695b64fbce6fb96d2cdd61b5f06a24624adc
ShuaichenLau <68754807@qq.com>
2020-01-02 20:57:48 +0800

    token 幂等 api demo

--- JavaEEDemo/bootDemo/src/main/java/com/alice/controller/HelloWorldController.java
+++ JavaEEDemo/bootDemo/src/main/java/com/alice/controller/HelloWorldController.java
@@ -1,8 +1,12 @@
 package com.alice.controller;
 
-import org.springframework.stereotype.Controller;
+import com.alice.utils.TokenUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Calendar;
 
 @RestController
 public class HelloWorldController {
@@ -12,5 +16,25 @@ public class HelloWorldController {
         return "helloWorld";
     }
 
+    @RequestMapping("/token")
+    public String getToken() {
+        return TokenUtils.getToken();
+    }
+
+
+    @RequestMapping(value = "/addOrder", produces = "application/json;charset=utf-8")
+    public String addOrder(HttpServletRequest request) {
+        String token = request.getHeader("token");
+        if (StringUtils.isEmpty(token)) {
+            return "参数错误";
+        }
+
+        if (!TokenUtils.findToken(token)) {
+            return "请勿重复提交!";
+        }
+
+        return Calendar.getInstance().getTime().toString();
+
+    }
 
 }
