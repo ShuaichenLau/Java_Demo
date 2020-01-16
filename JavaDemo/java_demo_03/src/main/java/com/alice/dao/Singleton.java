340534d142a1d5d7872baceaa5c00b47da0fc4c5
ShuaichenLau <68754807@qq.com>
2020-01-17 07:58:00 +0800

    双重校验锁

---
+++ JavaDemo/java_demo_03/src/main/java/com/alice/dao/Singleton.java
@@ -0,0 +1,23 @@
+package com.alice.dao;
+
+import java.io.Serializable;
+
+public class Singleton implements Serializable {
+
+    private volatile static Singleton singleton;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+
+}
